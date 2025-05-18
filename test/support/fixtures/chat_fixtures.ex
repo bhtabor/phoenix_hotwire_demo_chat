3f698635b41ddbcea19517bffa7df67890e96b46
Biruk Haileye Tabor <biruk.haileye@gmail.com>
2025-05-18 17:21:47 +0300

    mix phx.gen.html Chat Room rooms name:string

---
+++ test/support/fixtures/chat_fixtures.ex
@@ -0,0 +1,20 @@
+defmodule PhoenixHotwireDemoChat.ChatFixtures do
+  @moduledoc """
+  This module defines test helpers for creating
+  entities via the `PhoenixHotwireDemoChat.Chat` context.
+  """
+
+  @doc """
+  Generate a room.
+  """
+  def room_fixture(attrs \\ %{}) do
+    {:ok, room} =
+      attrs
+      |> Enum.into(%{
+        name: "some name"
+      })
+      |> PhoenixHotwireDemoChat.Chat.create_room()
+
+    room
+  end
+end
